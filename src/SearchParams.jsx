11184535f8b704463d58c0a3796cfb8ea31b8265
Raphael Nobre <nobre.roots@gmail.com>
2023-02-20 00:58:28 -0300

    Hook for location

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,13 +1,23 @@
-const SearchParmas = () => {
-    const location =  "Seattle, WA";
+import { useState } from "react";
+
+const SearchParams = () => {
+    const [location, setLocation] = useState("") 
     return (
         <div className="search-params">
             <form action="">
                 <label htmlFor="location">
                     Location
-                    <input id="location" value={location} placeholder="location"  />
+                    <input 
+                        onChange={e => setLocation(e.target.value)} 
+                        id="location" 
+                        value={location} 
+                        placeholder="location"  
+                    />
                 </label>
+                <button>Submit</button>
             </form>
         </div>
     )  
 }
+
+export default SearchParams
